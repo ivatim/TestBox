cde3ef6770fbf5b82b6ea1eebdd75adc1ba449bb
ivatim <ivantimkin92@mail.ru>
2017-08-17 14:44:15 +0300

    Initial commit

---
+++ src/StaticSuper.java
@@ -0,0 +1,11 @@
+public class StaticSuper {
+
+    static {
+        System.out.println("Родительский статический блок");
+    }
+    StaticSuper(){
+        System.out.println("Родительский конструктор");
+
+    }
+}
+
---
+++ src/StaticTest.java
@@ -0,0 +1,25 @@
+import java.util.Date;
+
+public class StaticTest extends StaticSuper {
+    static int rand;
+
+        static {
+            rand = (int) (Math.random()*6);
+            System.out.println("Статический блок"+rand);
+        }
+        StaticTest(){
+            System.out.println("Конструктор");
+        }
+        public static void main(String [] args) {
+            System.out.println("Внутри main");
+            StaticTest st =new StaticTest();
+        }
+}
+
+
+
+
+
+
+
+
